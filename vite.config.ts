915ad51156fc2259997cc039f25a10b65a9732c6
Neel Pandey <neelpandeyofficial@gmail.com>
2026-08-24 00:09:28 +0530

    update 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro({ preset: "vercel" }),
     react(),
     tailwindcss(),
   ],
